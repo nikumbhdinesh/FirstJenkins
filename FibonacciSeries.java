19bfd33030759a301f42cdc6f6730433c6f36739
Patil Dinesh <patildinesh@gmail.com>
2023-07-18 19:51:13 +0530

    Revised Code

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -3,32 +3,19 @@ import java.util.Scanner;
 public class FibonacciSeries {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the number of terms in the Fibonacci series: ");
         int numTerms = scanner.nextInt();
 
-        // Check if the number of terms is valid
-        if (numTerms <= 0) {
-            System.out.println("Invalid number of terms. Please enter a positive integer.");
-            return;
-        }
+        int firstTerm = 0, secondTerm = 1;
+        System.out.print("Fibonacci Series: " + firstTerm + " " + secondTerm);
 
-        System.out.println("Fibonacci series:");
-        int firstTerm = 0;
-        int secondTerm = 1;
-
-        // Display the first two terms
-        System.out.print(firstTerm + " " + secondTerm);
-
-        // Generate and display the remaining terms
         for (int i = 3; i <= numTerms; i++) {
             int nextTerm = firstTerm + secondTerm;
             System.out.print(" " + nextTerm);
-
-            // Update the values of firstTerm and secondTerm
             firstTerm = secondTerm;
             secondTerm = nextTerm;
         }
-        System.out.println();
 
         scanner.close();
     }
